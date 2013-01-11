c6d3c48c80226caf1c2d4d4e112c10a3e3095eda
bertrand keller <bertrand.keller@gmail.com>
2013-01-11 15:31:53 +0100

    page commit

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,2 +1,27 @@
-
 require 'jekyll/localization'
+
+module Jekyll
+  # restore standard paths
+  class Page
+
+    def url(lang = nil)
+      _localization_original_url
+    end
+
+    def destination(dest)
+      _localization_original_destination(dest)
+    end
+
+    def process(name)
+      _localization_original_process(name)
+    end
+    
+    # restore standard paths
+        %w[url destination process].each { |name|
+          alias_method name, "_localization_original_#{name}"
+    }
+
+  end
+end
+
+
