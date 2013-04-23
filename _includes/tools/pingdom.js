8c1c6094a38c449fbeebdbb4ac370192cdc0286c
bertrand keller <bertrand.keller@gmail.com>
2013-04-23 10:24:32 +0200

    pingdom

---
+++ _includes/tools/pingdom.js
@@ -0,0 +1,11 @@
+<script>
+var _prum = [['id', '51764493e6e53dd01f000000'],
+             ['mark', 'firstbyte', (new Date()).getTime()]];
+(function() {
+    var s = document.getElementsByTagName('script')[0]
+      , p = document.createElement('script');
+    p.async = 'async';
+    p.src = '//rum-static.pingdom.net/prum.min.js';
+    s.parentNode.insertBefore(p, s);
+})();
+</script>
