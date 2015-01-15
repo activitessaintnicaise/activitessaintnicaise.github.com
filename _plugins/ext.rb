2930c3215f636ddc02abcf46167c76fb06520549
bertrandkeller <bertrand.keller@gmail.com>
2015-01-15 09:26:37 +0100

    update

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,28 +1,28 @@
-require 'jekyll/localization'
-require 'jekyll/pagination'
+# require 'jekyll/localization'
+# require 'jekyll/pagination'
 
-module Jekyll
-  # restore standard paths
-  class Page
+# module Jekyll
+#   # restore standard paths
+#   class Page
 
-    def url(lang = nil)
-      _localization_original_url
-    end
+#     def url(lang = nil)
+#       _localization_original_url
+#     end
 
-    def destination(dest)
-      _localization_original_destination(dest)
-    end
+#     def destination(dest)
+#       _localization_original_destination(dest)
+#     end
 
-    def process(name)
-      _localization_original_process(name)
-    end
+#     def process(name)
+#       _localization_original_process(name)
+#     end
     
-    # restore standard paths
-        %w[url destination process].each { |name|
-          alias_method name, "_localization_original_#{name}"
-    }
+#     # restore standard paths
+#         %w[url destination process].each { |name|
+#           alias_method name, "_localization_original_#{name}"
+#     }
 
-  end
-end
+#   end
+# end
 
 
